fd26ec2bafd641de4184d42b97f0765da5c24a23
tbk03 <chrisjmartin03@gmail.com>
2022-12-16 14:44:20 +0000

    Update deployment process

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/iib-svelte-workshop-chart/'
+  base: '/inflation_chart_interactive/'
 })
